fd2884008979d240796f1dee866cc2ae07608895
natemaciel7 <natemaciel7@gmail.com>
2025-04-09 19:05:36 -0300

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000", // muda se seu backend estiver em outro lugar
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
